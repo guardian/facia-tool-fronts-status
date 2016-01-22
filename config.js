b92539c0837a84a33d275049736dde721c989e15
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-01-22 14:45:51 +0000

    fetch stale fronts

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    staleNetworkFront: 6 * 60 * 1000,
+    staleEditorialFront: 20 * 60 * 1000,
+    staleCommercialFront: 2.5 * 3600 * 1000,
+
+    networkFrontIds: ['uk', 'us', 'au', 'uk/sport', 'us/sport', 'au/sport']
+
+};
